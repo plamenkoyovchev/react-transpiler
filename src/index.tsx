3ebd2535cffee6040daf3b54e6cd7e31d23c4ab7
plamenkoyovchev <plamenkooo@gmail.com>
2021-01-26 19:21:28 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,53 @@
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const esBuildServiceRef = useRef<esbuild.Service>();
+
+  const startService = async () => {
+    esBuildServiceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmitClickedHandler = async () => {
+    if (!esBuildServiceRef.current) {
+      return;
+    }
+
+    const result = await esBuildServiceRef.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={onChangeHandler}></textarea>
+      <div>
+        <button onClick={onSubmitClickedHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
