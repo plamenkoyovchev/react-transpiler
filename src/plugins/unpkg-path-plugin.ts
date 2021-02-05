ec7970354ff641071303c06466f14ff9a5a034e2
plamenkoyovchev <plamenkooo@gmail.com>
2021-02-05 18:54:00 +0200

    Introduce cache mechanism

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
- 
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
@@ -37,12 +42,23 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // Check to see whether we've already cached the file
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        // store in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
